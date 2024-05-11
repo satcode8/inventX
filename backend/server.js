8eda8b51a6974f5e23508e5c1d43a66ebe361ce5
Sumedh Atreya Vedantham <sumedhatreya007@gmail.com>
2024-05-11 19:21:56 +0530

    Add all my files

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Error Middleware
+app.use(errorHandler);
+
+// Connect to DB and start server
+console.log(process.env.MONGO_URI);
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/inventory";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
